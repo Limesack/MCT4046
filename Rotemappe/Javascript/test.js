1e1f6c13fcec70eea4553c9cca93f502ce3f849e
Limesack <61227031+Limesack@users.noreply.github.com>
2021-03-18 16:34:12 +0100

    test

--- Rotemappe/Javascript/test.js
+++ Rotemappe/Javascript/test.js
@@ -1,8 +1,10 @@
-const synth = new Tone.Synth().toMaster();
-const now = Tone.now()
-// trigger the attack immediately
-synth.triggerAttack("C4", now)
-// wait one second before triggering the release
-synth.triggerRelease(now + 1)
+const synth = new Tone.Synth().toMaster()
+synth.triggerAttackRelease('C4', '8n')
 
-synth.start();
+document.getElementById("play-button").addEventListener("click", function() {
+  if (Tone.Transport.state !== 'started') {
+    Tone.Transport.start();
+  } else {
+    Tone.Transport.stop();
+  }
+});
