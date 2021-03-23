fec43992abcbc0e0128decc462c1b09c7fc87afe
Limesack <61227031+Limesack@users.noreply.github.com>
2021-03-23 10:27:24 +0100

    Github nettside

---
+++ Rotemappe/Javascript/synth.js
@@ -0,0 +1,25 @@
+const synth = new Tone.MonoSynth({
+	oscillator: {
+		type: "sawtooth",
+	},
+	envelope: {
+    attack: 0.005,
+    decay: 0.1,
+    release: 1,
+    sustain: 0.9,
+	},
+	filter: {
+    Q: 1,
+    rolloff: -12,
+    type: "lowpass",
+	},
+  filterEnvelope: {
+    attack: 0.6,
+    baseFrequency: 200,
+    decay: 0.2,
+    exponent: 2,
+    octaves: 3,
+    release: 2,
+    sustain: 0.5,
+  }
+}).toDestination();
