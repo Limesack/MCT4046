d72b27c77f762d0497662ce41a741ff9024d06b2
Limesack <61227031+Limesack@users.noreply.github.com>
2021-03-24 11:44:51 +0100

    synth 3

--- Javascript/app7.0.js
+++ Javascript/app7.0.js
@@ -200,8 +200,8 @@ const synth3 = new Tone.FMSynth({
 // SENDS SYNTH CHAIN TO OUPUT
 //synth.chain(chorus, ppdelay, reverb, gainNode1);
 //synth.chain(gainNode1);
-synth2.chain(gainNode2);
-//synth3.chain(gainNode3);
+//synth2.chain(gainNode2);
+synth3.chain(gainNode3);
 
 
 // synth1 - 6 autoFilter, synth 7-10 feedbackDelay, synth 11-16 autoFilter + automated sustain
